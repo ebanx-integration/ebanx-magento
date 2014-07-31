4bebe0bde6c468302e6cc41fe92645613d20f109
Gustavo Henrique Mascarenhas Machado <guhemama@gmail.com>
2014-07-31 15:19:26 -0300

    translated error messages

--- app/code/local/Ebanx/Ebanx/Model/Payment.php
+++ app/code/local/Ebanx/Ebanx/Model/Payment.php
@@ -228,7 +228,7 @@ class Ebanx_Ebanx_Model_Payment extends Mage_Payment_Model_Method_Abstract
             else
             {
                 Mage::log('Authorizing order [' . $order->getIncrementId() . '] - error: ' . $response->status_message);
-                Mage::throwException($response->status_message);
+                Mage::throwException($this->getEbanxErrorMessage($response->status_code));
             }
         }
         catch (Exception $e)
@@ -261,4 +261,109 @@ class Ebanx_Ebanx_Model_Payment extends Mage_Payment_Model_Method_Abstract
 
         return $this;
     }
+
+    /**
+   * Returns user friendly error messages
+   * @param  string $errorCode The error code
+   * @return string
+   */
+  protected function getEbanxErrorMessage($errorCode)
+  {
+      $errors = array(
+          "BP-DR-1"  => "O modo deve ser full ou iframe"
+        , "BP-DR-2"  => "É necessário selecionar um método de pagamento"
+        , "BP-DR-3"  => "É necessário selecionar uma moeda"
+        , "BP-DR-4"  => "A moeda não é suportada pelo EBANX"
+        , "BP-DR-5"  => "É necessário informar o total do pagamento"
+        , "BP-DR-6"  => "O valor do pagamento deve ser maior do que X"
+        , "BP-DR-7"  => "O valor do pagamento deve ser menor do que"
+        , "BP-DR-8"  => "O valor total somado ao valor de envio deve ser igual ao valor total"
+        , "BP-DR-13" => "É necessário informar um nome"
+        , "BP-DR-14" => "O nome não pode conter mais de 100 caracteres"
+        , "BP-DR-15" => "É necessário informar um email"
+        , "BP-DR-16" => "O email não pode conter mais de 100 caracteres"
+        , "BP-DR-17" => "O email informado é inválido"
+        , "BP-DR-18" => "O cliente está suspenso no EBANX"
+        , "BP-DR-19" => "É necessário informar a data de nascimento"
+        , "BP-DR-20" => "A data de nascimento deve estar no formato dd/mm/aaaa"
+        , "BP-DR-21" => "É preciso ser maior de 16 anos"
+        , "BP-DR-22" => "É necessário informar um CPF ou CNPJ"
+        , "BP-DR-23" => "O CPF informado não é válido"
+        , "BP-DR-24" => "É necessário informar um CEP"
+        , "BP-DR-25" => "É necessário informar o endereço"
+        , "BP-DR-26" => "É necessário informar o número do endereço"
+        , "BP-DR-27" => "É necessário informar a cidade"
+        , "BP-DR-28" => "É necessário informar o estado"
+        , "BP-DR-29" => "O estado informado é inválido. Deve se informar a sigla do estado (ex.: SP)"
+        , "BP-DR-30" => "O código do país deve ser 'br'"
+        , "BP-DR-31" => "É necessário informar um telefone"
+        , "BP-DR-32" => "O telefone informado é inválido"
+        , "BP-DR-33" => "Número de parcelas inválido"
+        , "BP-DR-34" => "Número de parcelas inválido"
+        , "BP-DR-35" => "Método de pagamento inválido: X"
+        , "BP-DR-36" => "O método de pagamento não está ativo"
+        , "BP-DR-39" => "CPF, nome e data de nascimento não combinam"
+        , "BP-DR-40" => "Cliente atingiu o limite de pagamentos para o período"
+        , "BP-DR-41" => "Deve-se escolher um tipo de pessoa - física ou jurídica."
+        , "BP-DR-42" => "É necessário informar os dados do responsável pelo pagamento"
+        , "BP-DR-43" => "É necessário informar o nome do responsável pelo pagamento"
+        , "BP-DR-44" => "É necessário informar o CPF do responsável pelo pagamento"
+        , "BP-DR-45" => "É necessário informar a data de bascunebti do responsável pelo pagamento"
+        , "BP-DR-46" => "CPF, nome e data de nascimento do responsável não combinam"
+        , "BP-DR-47" => "A conta bancário deve conter no máximo 10 caracteres"
+        , "BP-DR-48" => "É necessário informar os dados do cartão de crédito"
+        , "BP-DR-49" => "É necessário informar o número do cartão de crédito"
+        , "BP-DR-51" => "É necessário informar o nome do titular do cartão de crédito"
+        , "BP-DR-52" => "O nome do titular do cartão deve conter no máximo 50 caracteres"
+        , "BP-DR-54" => "É necessário informar o CVV do cartão de crédito"
+        , "BP-DR-55" => "O CVV deve conter no máximo 4 caracteres"
+        , "BP-DR-56" => "É necessário informar a data de venciomento do cartão de crédito"
+        , "BP-DR-57" => "A data de vencimento do cartão de crédito deve estar no formato dd/mm/aaaa"
+        , "BP-DR-58" => "A data de vencimento do cartão é inválida"
+        , "BP-DR-59" => "A data de vencimento do cartão é inválida"
+        , "BP-DR-61" => "Não foi possível criar um token para este cartão de crédito"
+        , "BP-DR-62" => "Pagamentos recorrentes não estão habilitados para este merchant"
+        , "BP-DR-63" => "Token não encontrado para este adquirente"
+        , "BP-DR-64" => "Token não encontrado"
+        , "BP-DR-65" => "O token informado já está sendo utilizado"
+        , "BP-DR-66" => "Token inválido. O token deve ter entre 32 e 128 caracteres"
+        , "BP-DR-67" => "A data de venciomento do cartão de crédito é inválida"
+        , "BP-DR-68" => "É necessário informar o número da conta bancária"
+        , "BP-DR-69" => "A conta bancária não pode conter mais de 10 caracteres"
+        , "BP-DR-70" => "É necessário informar a agência bancária"
+        , "BP-DR-71" => "O código do banco não pode ter mais de 5 caracteres"
+        , "BP-DR-72" => "É necessário informar o código do banco"
+        , "BP-DR-73" => "É necessário informar os dados da conta para débito em conta"
+        , "BP-R-1" => "É necessário informar a moeda"
+        , "BP-R-2" => "É necessário informar o valor do pagamento"
+        , "BP-R-3" => "É necessário informar o código do pedido"
+        , "BP-R-4" => "É necessário informar o nome"
+        , "BP-R-5" => "É necessário informar o email"
+        , "BP-R-6" => "É necessário selecionar o método de pagamento"
+        , "BP-R-7" => "O método de pagamento não está ativo"
+        , "BP-R-8" => "O método de pagamento é inválido"
+        , "BP-R-9" => "O valor do pagamento deve ser positivo: X"
+        , "BP-R-10" => "O valor do pagamento deve ser maior do que X"
+        , "BP-R-11" => "O método de pagamento não suporta parcelamento"
+        , "BP-R-12" => "O número máximo de parcelas é X. O valor informado foi de X parcelas."
+        , "BP-R-13" => "O valor mínimo das parcelas é de R$ X."
+        , "BP-R-17" => "O pagamento não está aberto"
+        , "BP-R-18" => "O típo de pessoa é inválido"
+        , "BP-R-19" => "O checkout com CNPJ não está habilitado"
+        , "BP-R-20" => "A data de vencimento deve estar no formato dd/mm/aaaa"
+        , "BP-R-21" => "A data de vencimento é inválida"
+        , "BP-R-22" => "A data de vencimento é inválida"
+        , "BP-R-23" => "A moeda não está ativa no sistema"
+        , "BP-ZIP-1" => "O CEP não foi informado"
+        , "BP-ZIP-2" => "O CEP não é válido"
+        , "BP-ZIP-3" => "O endereço não pode ser encontrado"
+      );
+
+      if (array_key_exists($errorCode, $errors))
+      {
+          return $errors[$errorCode];
+      }
+
+      return 'Ocorreu um erro desconhecido. Por favor contacte o administrador.';
+  }
 }
---
+++ app/code/local/Ebanx/Ebanx/sql/ebanx_setup/install-1.6.0.php
@@ -0,0 +1,98 @@
+<?php
+
+$this->startSetup();
+
+$this->addAttribute('order_payment', 'ebanx_hash', array(
+    'type'            => 'text'
+  , 'backend_type'    => 'text'
+  , 'frontend_input'  => 'text'
+  , 'is_user_defined' => 0
+  , 'label'           => 'EBANX hash'
+  , 'visible'         => 1
+  , 'required'        => 0
+  , 'user_defined'    => 0
+  , 'searchable'      => 1
+  , 'filterable'      => 0
+  , 'comparable'      => 0
+  , 'default'         => ''
+));
+
+$this->addAttribute('customer', 'ebanx_cpf', array(
+    'type'             => 'text'
+  , 'input'            => 'text'
+  , 'label'            => 'EBANX CPF'
+  , 'visible'          => 1
+  , 'is_user_defined'  => 0
+  , 'required'         => 0
+  , 'visible_on_front' => 0
+  , 'global'           => 1
+  , 'default'          => ''
+  , 'adminhtml_only'   => 1
+  , 'source'           => NULL
+));
+
+$this->addAttribute('customer', 'ebanx_birthdate', array(
+    'type'             => 'text'
+  , 'input'            => 'text'
+  , 'label'            => 'EBANX birthdate'
+  , 'visible'          => 1
+  , 'is_user_defined'  => 0
+  , 'required'         => 0
+  , 'visible_on_front' => 0
+  , 'global'           => 1
+  , 'default'          => ''
+  , 'adminhtml_only'   => 1
+  , 'source'           => NULL
+));
+
+try
+{
+  $this->run("
+    INSERT INTO `{$this->getTable('directory/country_region')}`
+    (`country_id`,`default_name`,`code`) VALUES
+    ('BR','Acre','AC'),
+    ('BR','Alagoas','AL'),
+    ('BR','Amapá','AP'),
+    ('BR','Amazonas','AM'),
+    ('BR','Bahia','BA'),
+    ('BR','Ceará','CE'),
+    ('BR','Distrito Federal','DF'),
+    ('BR','Espírito Santo','ES'),
+    ('BR','Goiás','GO'),
+    ('BR','Maranhão','MA'),
+    ('BR','Mato Grosso','MT'),
+    ('BR','Mato Grosso do Sul','MS'),
+    ('BR','Minas Gerais','MG'),
+    ('BR','Pará','PA'),
+    ('BR','Paraíba','PB'),
+    ('BR','Paraná','PR'),
+    ('BR','Pernambuco','PE'),
+    ('BR','Piauí','PI'),
+    ('BR','Rio de Janeiro','RJ'),
+    ('BR','Rio Grande do Norte','RN'),
+    ('BR','Rio Grande do Sul','RS'),
+    ('BR','Rondônia','RO'),
+    ('BR','Roraima','RR'),
+    ('BR','Santa Catarina','SC'),
+    ('BR','São Paulo','SP'),
+    ('BR','Sergipe','SE'),
+    ('BR','Tocantins','TO');
+
+    INSERT INTO `{$this->getTable('directory/country_region_name')}`
+    (`locale`,`region_id`,`name`)
+    SELECT
+      'en_US',
+      `region_id`,
+      `default_name`
+    FROM
+      `{$this->getTable('directory/country_region')}`
+    WHERE
+      `country_id` = 'BR';
+  ");
+}
+catch(Exception $e)
+{
+  Mage::logException($e);
+}
+
+$this->endSetup();
